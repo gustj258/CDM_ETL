bfeba0d27cdc6a39b7520243091743ed91bf04c3
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-10-25 11:00:03 +0900

    Update 30.Condition_era.sql

--- 30.Condition_era.sql
+++ 30.Condition_era.sql
@@ -2,20 +2,20 @@
  --encoding : UTF-8
  --Author: OHDSI
   
-@cohort_cdm : DB containing NHIS National Sample cohort DB
-@ResultDatabaseSchema : DB for NHIS-NSC in CDM format
+cohort_cdm : DB containing NHIS National Sample cohort DB
+cohort_cdm : DB for NHIS-NSC in CDM format
  
  --Description: OHDSI에서 생성한 condition_era 생성 쿼리
                총 3개의 temp table이 만들어짐
-			   1) #cteConditionTarget
-			   2) #cteEndDates
-			   3) #cteConditionEnds
+			   1) cteConditionTarget
+			   2) cteEndDates
+			   3) cteConditionEnds
  --Generating Table: CONDITION_ERA
 ***************************************/
 
 /**************************************
  @Author: OHDSI
- @Date: 2017.02.21 실행
+ @Date: 2019.10.05
  
  @Database: NHIS_NSC (16호 서버)
  @Description: OHDSI에서 생성한 condition_era 생성 쿼리
@@ -29,24 +29,13 @@
  1. condition_era 테이블 생성
 ***************************************/ 
 CREATE TABLE cohort_cdm.CONDITION_ERA  (
-     condition_era_id				NUMBER NOT NULL , 
-     person_id							NUMBER NOT NULL ,
-     condition_concept_id			NUMBER NOT NULL ,
-     condition_era_start_date			DATE NOT NULL ,
-     condition_era_end_date				DATE 	NOT NULL ,
-     condition_occurrence_count			NUMBER NULL 
-);
-
--- Generate ID using sequence and trigger
-CREATE SEQUENCE cohort_cdm.CONDITION_ERA_seq START WITH 1 INCREMENT BY 1;
-
-CREATE OR REPLACE TRIGGER cohort_cdm.CONDITION_ERA_seq_tr
- BEFORE INSERT ON cohort_cdm.CONDITION_ERA FOR EACH ROW
- WHEN (NEW.condition_era_id IS NULL)
-BEGIN
- SELECT cohort_cdm.CONDITION_ERA_seq.NEXTVAL INTO :NEW.condition_era_id FROM DUAL;
-END;
-/ 
+     condition_era_id					INTEGER	 identity(1,1)    NOT NULL , 
+     person_id							INTEGER     NOT NULL ,
+     condition_concept_id				INTEGER   NOT NULL ,
+     condition_era_start_date			DATE      NOT NULL ,
+     condition_era_end_date				DATE 	  NOT NULL ,
+     condition_occurrence_count			INTEGER			NULL 
+); 
 
 	 
 /**************************************
@@ -58,7 +47,7 @@ SELECT
 	person_id, 
 	condition_concept_id, 
 	condition_start_date, 
-	COALESCE(NULLIF(condition_end_date,NULL), interval '31'  day + condition_start_date) AS condition_end_date
+	COALESCE(NULLIF(condition_end_date,NULL), dateadd (day, 31, condition_start_date)) AS condition_end_date
 into cteConditionTarget 
 FROM condition_occurrence;
 	
@@ -67,7 +56,7 @@ FROM condition_occurrence;
 SELECT
 	person_id
 	, condition_concept_id
-	, interval '-30' day + event_date AS end_date 
+	, dateadd(day, -30, event_date) AS end_date 
 into cteEndDates FROM
 (
 	SELECT
@@ -75,7 +64,7 @@ into cteEndDates FROM
 		, condition_concept_id
 		, event_date
 		, event_type
-		, MAX(start_ordinal) OVER FROM dual (PARTITION BY person_id, condition_concept_id ORDER BY event_date, event_type ROWS UNBOUNDED PRECEDING) AS start_ordinal -- this pulls the current START down from the prior rows so that the NULLs from the END DATES will contain a value we can compare with 
+		, MAX(start_ordinal) OVER (PARTITION BY person_id, condition_concept_id ORDER BY event_date, event_type ROWS UNBOUNDED PRECEDING) AS start_ordinal -- this pulls the current START down from the prior rows so that the NULLs from the END DATES will contain a value we can compare with 
 		, ROW_NUMBER() OVER (PARTITION BY person_id, condition_concept_id ORDER BY event_date, event_type) AS overall_ord -- this re-numbers the inner UNION so all rows are numbered ordered by the event date
 	FROM
 	(
@@ -86,13 +75,15 @@ into cteEndDates FROM
 			, condition_start_date AS event_date
 			, -1 AS event_type
 			, ROW_NUMBER() OVER (PARTITION BY person_id, condition_concept_id ORDER BY condition_start_date) AS start_ordinal
-		FROM cteConditionTarget UNION ALL
+		FROM cteConditionTarget
+	
+		UNION ALL
 	
 		-- pad the end dates by 30 to allow a grace period for overlapping ranges.
 		SELECT
 			person_id
 				, condition_concept_id
-			, interval '30'  day +condition_end_date 
+			, dateadd( day,30,condition_end_date) 
 			, 1 AS event_type
 			, NULL
 		FROM cteConditionTarget
@@ -120,7 +111,7 @@ GROUP BY
  3. 2단계: condition_era에 데이터 입력
 ***************************************/ 
 
-INSERT INTO cohort_cdm.condition_era(person_id, condition_concept_id, condition_era_start_date, condition_era_end_date, condition_occurrence_count)
+INSERT INTO cohort_cdm.condition_era (person_id, condition_concept_id, condition_era_start_date, condition_era_end_date, condition_occurrence_count)
 SELECT
 	person_id
 	, condition_concept_id
